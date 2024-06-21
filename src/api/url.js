99a29515de124c7349db23f7483f46ef1f3303c6
ashinat24 <153436289+ashinat24@users.noreply.github.com>
2024-06-21 22:09:35 +0530

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://medicobackend-bl90.onrender.com";
+ export const BASE_URL = "http://localhost:5173";
+// export const BASE_URL = "https://medicobackend-bl90.onrender.com";
